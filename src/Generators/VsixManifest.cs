7b051b6af2728f0f768fd69664207f9056aa1a79
Brian Hart <brian.hart@xylogix.net>
2022-01-20 16:45:04 -0600

    Update VsixManifest.cs
    * Figured while I am here, use CodeMaid to rearrange the properties of the VsixManifest class in alphabetical order to make them easier to find.

--- src/Generators/VsixManifest.cs
+++ src/Generators/VsixManifest.cs
@@ -2,19 +2,19 @@
 {
     internal class VsixManifest
     {
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Language { get; set; }
         public string Author { get; set; }
-        public string Version { get; set; }
-        public string Icon { get; set; }
-        public string Preview { get; set; }
-        public string Tags { get; set; }
-        public string License { get; set; }
+        public string Description { get; set; }
         public string GettingStartedUrl { get; set; }
-        public string ReleaseNotesUrl { get; set; }
+        public string Icon { get; set; }
+        public string ID { get; set; }
+        public string Language { get; set; }
+        public string License { get; set; }
         public string MoreInfoUrl { get; set; }
+        public string Name { get; set; }
+        public string Preview { get; set; }
+        public string ReleaseNotesUrl { get; set; }
+        public string Tags { get; set; }
+        public string Version { get; set; }
 
         public override string ToString()
         {
