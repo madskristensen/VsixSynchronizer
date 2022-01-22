cde83cc27e313f9ee610a548fdf5e3c0363bbd5d
Hazel Megan <matt@mrlacey.co.uk>
2022-01-22 14:33:56 +0000

    Add platform specific marketplace links to generated files

---
+++ src/Generators/VsVersionHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.Setup.Configuration;
+
+namespace VsixSynchronizer
+{
+    public static class VsVersionHelper
+    {
+        public static bool IsVs2019()
+        {
+            ISetupConfiguration configuration = new SetupConfiguration();
+            ISetupInstance instance = configuration.GetInstanceForCurrentProcess();
+            var _vsProductVersion = instance.GetInstallationVersion();
+
+            return _vsProductVersion.StartsWith("16.");
+        }
+    }
+}
